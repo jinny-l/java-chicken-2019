030d683be3ce902f516ccd65d8a6939bab2a966b
jinny-l <jinny.l.dev@gmail.com>
2023-12-06 17:12:54 +0900

    feat: 어플리케이션 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,14 @@
+import Controller.OrderController;
+import Controller.PaymentController;
+import Controller.PosController;
+
 public class Application {
 
     public static void main(String[] args) {
-
+        PosController controller = new PosController(
+                new OrderController(),
+                new PaymentController()
+        );
+        controller.run();
     }
 }
